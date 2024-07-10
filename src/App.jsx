9328d997684cb2f623c0292728008012651f334e
{Anwar <anwarelghazi8596@gmail.com>
2024-07-10 16:07:44 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+// src/App.jsx
+import React, { useState } from "react";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import SearchBar from "./components/SearchBar";
 
-import Home from "./Home"
-import Navbar from "./Navbar"
 function App() {
-  
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
     <>
-    <Home/>
-    <Navbar/>
+      <SearchBar onSearch={handleSearch} />
+      <Home searchQuery={searchQuery} />
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
