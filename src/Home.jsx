c5e928469a9f30effac8e3bee8bac919c5e76df8
wamweaa <patokinya12@gmail.com>
2024-07-10 01:35:00 -0700

    home page modifications

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import { FaCartShopping } from "react-icons/fa6";
 function Home() {
   return (
-    <div>
-      {/* background image  */}
+    <div className='home-container'>
+          <div>
+        <nav>
+            <ul className='navbar'>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#fruits">Fruits</a>
+                </li>
+                <li>
+                    <a href="#vegetables">Vegetables</a>
+                </li>
+                <li>
+                    <a href="#meats">Meats</a>
+                </li>
+                <li>
+                    <a href="#gains">Grains</a>
+                </li>
+                <li>
+                    <a href="#cart"><FaCartShopping /></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div className='home-container'>
+      <img src="https://i.pinimg.com/564x/7a/af/d9/7aafd92b142fb07d4ec28988978f6c9a.jpg" 
+      alt="background"
+      className='home-image' 
+      />
+    </div>
+
     </div>
   )
 }
