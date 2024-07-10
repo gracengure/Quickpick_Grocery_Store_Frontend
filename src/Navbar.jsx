352b9eea0188009270464c03bf8899e8ed5efd88
wamweaa <patokinya12@gmail.com>
2024-07-10 04:33:31 -0700

    adding drop down

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import { FaCartShopping } from 'react-icons/fa6';
+import { FaCaretDown } from 'react-icons/fa';
 function Navbar() {
+const[isDropdown ,setIsDropdown]=useState(false)
+function toggleDown(){
+  setIsDropdown(!isDropdown)
+}
   return (
     <div className="home-container">
      <nav>
         <ul className="navbar">
           <li>
-            <a href="#home">Home</a>
+            <a href="#">Home</a>
           </li>
           <li>
-            <a href="#categories">Categories</a>
-            <ul className="dropdown">
+         Categories
+            <FaCaretDown onClick={toggleDown}/>
+            { isDropdown &&(<ul className="dropdown">
               <li><a href="#fruits">Fruits</a></li>
               <li><a href="#vegetables">Vegetables</a></li>
               <li><a href="#meats">Meats</a></li>
               <li><a href="#grains">Grains</a></li>
-            </ul>
+            </ul>)}
           </li>
           <li>
             <a href="#about">About</a>
